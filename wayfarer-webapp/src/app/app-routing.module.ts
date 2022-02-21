c290dada612635da35a02ea9be5cdaf1d792341d
Toni-Ty <Antonique2479@yahoo.com>
2022-02-21 15:25:54 -0600

    created user travel review form for posts in post-form component, added error validation, routed to cities page

--- wayfarer-webapp/src/app/app-routing.module.ts
+++ wayfarer-webapp/src/app/app-routing.module.ts
@@ -5,20 +5,18 @@ import { CitiesComponent } from './components/cities/cities.component';
 import { PostsComponent } from './posts/posts.component';
 // import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
-
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'cities', component: CitiesComponent},
-  { path: 'cities/:id', component: PostsComponent},
-  { path: 'cities/:id/posts/:id', component: PostsComponent},
+  { path: 'cities', component: CitiesComponent },
+  { path: 'cities/:id', component: PostsComponent },
+  { path: 'cities/:id/posts/:id', component: PostsComponent },
   { path: 'search/:city-search', component: HomeComponent },
-     // redirect to home
-     { path: '**', redirectTo: '' }
+  // redirect to home
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- wayfarer-webapp/src/app/app.module.ts
+++ wayfarer-webapp/src/app/app.module.ts
@@ -1,24 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 //import { GaugeModule } from 'angular-gauge';
 
-
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
 import { CitiesComponent } from './components/cities/cities.component';
 import { PostsComponent } from './posts/posts.component';
-
-
-
-
-
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +22,8 @@ import { PostsComponent } from './posts/posts.component';
     SearchBarComponent,
     HomeComponent,
     CitiesComponent,
-    PostsComponent
+    PostsComponent,
+    PostFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,10 +31,11 @@ import { PostsComponent } from './posts/posts.component';
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
     //GaugeModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
